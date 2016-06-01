ce3e26cfd9021576911be1dd31a768a7cd5be6cd
vjdodge <valoriejones@gmail.com>
2016-05-31 18:19:45 -0700

    Fix api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ var blocmetrics = {};
 blocmetrics.report = function(eventName) {
    var event = {event: { name: eventName }};
    var request = new XMLHttpRequest();
-   request.open("POST", "http://stark-caverns-21277.herokuapp.com/", true);
+   request.open("POST", "http://stark-caverns-21277.herokuapp.com/api/events", true);
    request.setRequestHeader('Content-Type', 'application/json');
    request.send(JSON.stringify(event));
 }
